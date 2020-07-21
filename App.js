49c1cd31b41f1a4161d8eb6c740a6813d1d069a1
Enrique Lopez <elopez@digitalgeko.com>
2020-07-20 22:56:07 -0600

    lib
    Picker

--- App.js
+++ App.js
@@ -4,21 +4,39 @@ import {
     StyleSheet,
     ScrollView,
     View,
-    Text,
+    Image,
     StatusBar,
 } from 'react-native';
 
 import Header from './components/header/header';
+import Form from "./components/form/form";
 
 
 const App = () => {
     return (
         <>
-            <Header></Header>
+            <Header/>
+            <Image
+                style={styles.image}
+                source={require('./assets/img/cryptomonedas.png')}
+            />
+            <View style={styles.container}>
+                <Form/>
+            </View>
+
         </>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 150,
+        marginHorizontal: '2.5%'
+    },
+    container: {
+        marginHorizontal: '2.5%'
+    }
+});
 
 export default App;
--- components/form/form.js
+++ components/form/form.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     StyleSheet,
     View,
     Text,
-    Platform,
 } from 'react-native';
+import {Picker} from '@react-native-community/picker'
 
 const Form = () => {
 
+    const [currency, saveCurrency] = useState('');
+    const [cryptocurrencies, saveCryptocurrencies] = useState('');
+
+    const getCurrency = currency => {
+        saveCurrency(currency)
+    }
+
+    return (
+        <View>
+            <Text style={styles.label}>
+                Currency
+            </Text>
+            <Picker
+                selectedValue={currency}
+                onValueChange={currency => getCurrency(currency)}
+            >
+                <Picker.Item label='- Select -' value=''/>
+                <Picker.Item label='Dollar' value='USD'/>
+                <Picker.Item label='Mexican peso' value='MXN'/>
+                <Picker.Item label='Euro' value='EUR'/>
+                <Picker.Item label='Pound Sterling' value='GBP'/>
+            </Picker>
+            <Text style={styles.label}>
+                Cryptocurrencies
+            </Text>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    label: {
+        fontFamily: 'Lato-Black',
+        textTransform: 'uppercase',
+        fontSize: 22,
+        marginVertical: 20
+    }
+});
 
 export default Form;
