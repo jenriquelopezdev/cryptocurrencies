e70cf3fa29369f60f56f81a91abf4193cc2b6a9d
Enrique Lopez <elopez@digitalgeko.com>
2020-07-20 22:28:27 -0600

    header

--- components/header/header.js
+++ components/header/header.js
@@ -15,7 +15,14 @@ const Header = () => (
 const styles = StyleSheet.create({
     header: {
         paddingTop: Platform.OS === 'ios' ? 50 : 10,
-        fontFamily: 'Lato-Black'
+        fontFamily: 'Lato-Black',
+        backgroundColor: '#5E49E2',
+        paddingBottom: 10,
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        fontSize: 20,
+        color: '#fff',
+        marginBottom: 30
     }
 });
 
