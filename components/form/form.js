467dfdd9322e4392931895d7562117467f0059f1
Enrique Lopez <elopez@digitalgeko.com>
2020-07-20 22:09:00 -0600

    init components

---
+++ components/form/form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    Platform,
+} from 'react-native';
+
+const Form = () => {
+
+}
+
+const styles = StyleSheet.create({});
+
+export default Form;
---
+++ components/header/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    Platform,
+} from 'react-native';
+
+const Header = () => {
+
+}
+
+const styles = StyleSheet.create({});
+
+export default Header;
